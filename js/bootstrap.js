410a9d08327e17fe26fd0c2c3a930d9ee7b0b1f3
Luiz Socrate <luiz@socrate.com.br>
2012-05-18 01:23:09 -0300

    Basic interface for adding players

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -13,6 +13,51 @@ yepnope([
     },
     complete: function () {
       window.tournament = new SwissTournament();
+      $(function($){
+        var $ranking = $('.ranking');
+
+        $('.js-button').on('click', function (ev) {
+          if ($(this).data('action') === 'addPlayer') {
+            var playerName = prompt('Player name');
+            var playerClan = prompt('Player clan');
+
+            tournament.addPlayer(playerName, playerClan);
+            tournament.updateRanking();
+
+            $ranking.find('.ranking-row').remove();
+            $.each(tournament.players, function (index, player) {
+              var row = $('<tr/>', {
+                class:'ranking-row',
+                data: {
+                  playerId:player.id
+                }
+              });
+              row.append($('<td/>', {
+                class:'ranking-header ranking-player',
+                data: {
+                  attribute: 'name'
+                },
+                text: player.name
+              }));
+              row.append($('<td/>', {
+                class:'ranking-header ranking-clan',
+                data: {
+                  attribute: 'clan'
+                },
+                text: player.clan
+              }));
+              row.append($('<td/>', {
+                class:'ranking-header ranking-points',
+                data: {
+                  attribute: 'points'
+                },
+                text: player.points
+              }));
+              $ranking.append(row);
+            });
+          }
+        });
+      });
     }
   },
 ]);
