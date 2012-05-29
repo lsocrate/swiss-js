d806db25181ea15ebee37b987ac01e07ca185875
Luiz Socrate <luiz@socrate.com.br>
2012-05-29 18:29:50 -0300

    Namespacing

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -3,18 +3,22 @@ yepnope([
     load: {
       'jquery':'http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js',
       'swiss': 'js/swiss.js',
-      'swiss-ui': 'js/swiss-ui.js'
+      'swiss-ui': 'js/swiss-ui.js',
     },
     callback: {
       'jquery' : function (url, result, key) {
         if (!window.jQuery) {
           yepnope('js/libs/jquery-1.7.2.min.js');
         }
+      },
+      'swiss' : function () {
+      },
+      'swiss-ui': function () {
       }
     },
     complete: function () {
       $(function($){
-        window.tournament = new SwissTournament();
+        window.tournament = new SwissJS.Tournament();
         window.tournamentUI = new TournamentUI(window.tournament);
       });
     }
