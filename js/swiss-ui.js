a510adc8e1f51225a373db568a665bb2d262e6b7
Luiz Socrate <luiz@socrate.com.br>
2012-06-06 19:15:17 -0300

    Basic ui for generating round

--- js/swiss-ui.js
+++ js/swiss-ui.js
@@ -1,7 +1,8 @@
 var TournamentUI = function (tournament){
   this.tournament = tournament;
   this.nav        = {};
-  this.ranking    = $('.ranking');
+  this.ranking    = $('table.ranking');
+  this.round      = $('table.round');
 
   var UI = this;
   $('nav').on('click', 'button', function (ev) {
@@ -9,7 +10,9 @@ var TournamentUI = function (tournament){
     UI[action]();
   });
 
-  $(this.tournament).on('rankUpdated', this.updateRanking);
+  $(this.tournament).on('rankUpdated', function () {
+    UI.updateRanking();
+  });
 };
 
 TournamentUI.prototype.addPlayer = function () {
@@ -21,6 +24,30 @@ TournamentUI.prototype.addPlayer = function () {
   var playerClan = prompt('Player clan');
 
   this.tournament.addPlayer(playerName, playerClan);
+};
+TournamentUI.prototype.startRound = function() {
+  this.tournament.generateRound();
+  var roundMatches = this.tournament.getRound().matches;
+  var UI = this;
+
+  this.round.find('.round-row').remove();
+  $.each(roundMatches, function (index, match) {
+    var row = $('<tr/>', {
+      class:'round-row'
+    });
+
+    $.each(match.players, function (index, player) {
+      row.append($('<td/>', {
+        class:'round-player',
+        data: {
+          attribute: 'player'
+        },
+        text: player.name + ' - ' + player.clan
+      }));
+    });
+
+    UI.round.append(row);
+  });
 };
 TournamentUI.prototype.updateRanking = function() {
   var UI = this;
