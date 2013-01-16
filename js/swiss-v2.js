e64fc0997321a8497da119167bd61a3dc08b1bc0
Luiz Sócrate <luiz@socrate.com.br>
2013-01-16 17:08:21 -0200

    addMatch

--- js/swiss-v2.js
+++ js/swiss-v2.js
@@ -3,9 +3,13 @@ var SwissTournament = SwissTournament || {}
 SwissTournament = function() {
   var playerCount = 0
   this.players = {}
+  this.matches = {}
 
   var nextPlayerId = function () {
     return ++playerCount
+  }
+  var makeMatchName = function (id1, id2) {
+    return "m" + id1 + "@" + id2
   }
 
   var Player = function (name, clan) {
@@ -15,6 +19,11 @@ SwissTournament = function() {
     this.points = 0
     this.opponents = {}
   }
+  var Match = function (player1, player2) {
+    this.players = {}
+    this.players["id" + player1.id] = player1
+    this.players["id" + player2.id] = player2
+  }
 
   this.addPlayer = function (name, clan) {
     var player = new Player(name, clan)
@@ -23,5 +32,12 @@ SwissTournament = function() {
   }
   this.getPlayer = function (id) {
     return this.players["id" + id]
+  }
+  this.addMatch = function (playerId1, playerId2) {
+    var player1 = this.getPlayer(playerId1)
+    var player2 = this.getPlayer(playerId2)
+    var match = new Match(player1, player2)
+
+    this.matches[makeMatchName(playerId1, playerId2)] = match
   }
 }
--- tests/tests/tests-v2.js
+++ tests/tests/tests-v2.js
@@ -13,3 +13,23 @@ test("Get player", function () {
   var player = this.tournament.getPlayer(1)
   ok(player)
 });
+
+module('Matches', {
+  setup: function () {
+    this.tournament = new SwissTournament();
+    this.tournament.addPlayer('Anna', 'Crab')
+    this.tournament.addPlayer('Bob', 'Crane')
+    this.tournament.addPlayer('Claude', 'Dragon')
+    this.tournament.addPlayer('Dennis', 'Lion')
+    this.tournament.addPlayer('Eliot', 'Mantis')
+    this.tournament.addPlayer('Francis', 'Phoenix')
+    this.tournament.addPlayer('George', 'Scorpion')
+    this.tournament.addPlayer('Henry', 'Spider');
+    this.tournament.addPlayer('Irvine', 'Unaligned');
+    this.tournament.addPlayer('Juliet', 'Unaligned');
+  }
+});
+test("Add match", function () {
+  this.tournament.addMatch(1, 2)
+  ok(this.tournament.matches["m1@2"])
+});
